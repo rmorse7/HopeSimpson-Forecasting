1ee513c46ba5c3a46f7f142f3d5849714ff2381f
NUMBKV <numbkv@gmail.com>
2021-12-12 01:08:17 -0600

    Initial Commit

--- web_app/src/orders.js
+++ web_app/src/orders.js
@@ -1,33 +1,44 @@
 import * as React from 'react';
 import Copyright from "./copyright";
 import Toolbar from "@mui/material/Toolbar";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import Deposits from "./Dashboard/deposits";
-import Chart from "./Dashboard/chart";
 import Box from "@mui/material/Box";
 import {Typography} from "@mui/material";
+import tableau from 'tableau-api';
+import {Component} from "react";
 
-export default function Orders() {
-    return (
-        <Box
-            component="main"
-            sx={{
-                backgroundColor: (theme) =>
-                    theme.palette.mode === 'light'
-                        ? theme.palette.grey[100]
-                        : theme.palette.grey[900],
-                flexGrow: 1,
-                height: '100vh',
-                overflow: 'auto',
-            }}
-        >
-            <Toolbar />
-            <Typography>
-                left blank for orders
-            </Typography>
-            <Copyright />
-        </Box>
-    )
+class Orders extends Component{
+    componentDidMount() {
+        this.initViz()
+    }
+
+    initViz() {
+        const vizUrl = 'https://prod-useast-b.online.tableau.com/t/minghaoweb/views/TexasDataDailyVisualize/Sheet1?:showAppBanner=false&:display_count=n&:showVizHome=n&:origin=viz_share_link';
+        const vizContainer = this.vizContainer;
+        let viz = new window.tableau.Viz(vizContainer, vizUrl)
+    }
+
+    render(){
+        return (
+            <Box
+                component="main"
+                sx={{
+                    backgroundColor: (theme) =>
+                        theme.palette.mode === 'light'
+                            ? theme.palette.grey[100]
+                            : theme.palette.grey[900],
+                    flexGrow: 1,
+                    height: '100vh',
+                    overflow: 'auto',
+                }}
+            >
+                <Toolbar />
+                <div ref={(div) => {
+                    this.vizContainer = div
+                }}/>
+                <Copyright />
+            </Box>
+        )
+    }
 }
+
+export default Orders;
